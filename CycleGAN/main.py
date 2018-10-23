470e22416398c413fbde8c40b4d19d9c8e6b1101
David Wajngot <davwa@cmiv.local>
2018-10-23 17:15:17 +0200

    can decrease filter sizes to increase batch size

--- CycleGAN/main.py
+++ CycleGAN/main.py
@@ -10,14 +10,15 @@ parser.add_argument('--epoch', dest='epoch', type=int, default=61, help='# of ep
 #Hyperparameter
 parser.add_argument('--epoch_step', dest='epoch_step', type=int, default=100, help='# of epoch to decay lr')
 #Hyperparameter
-parser.add_argument('--batch_size', dest='batch_size', type=int, default=4, help='# images in batch')
-parser.add_argument('--train_size', dest='train_size', type=int, default=1e8, help='# images used to train')
+parser.add_argument('--batch_size', dest='batch_size', type=int, default=2, help='# images in batch')
+parser.add_argument('--train_size', dest='train_size', type=int, default=300, help='# images used to train')
+#parser.add_argument('--train_size', dest='train_size', type=int, default=1e8, help='# images used to train')
 #Change to save RAM?
 parser.add_argument('--load_size', dest='load_size', type=int, default=286, help='scale images to this size')
 parser.add_argument('--fine_size', dest='fine_size', type=int, default=256, help='then crop to this size')
-#Hyperparameter
+#Hyperparameter. can be decreased to save RAM
 parser.add_argument('--ngf', dest='ngf', type=int, default=64, help='# of gen filters in first conv layer')
-#Hyperparameter
+#Hyperparameter. can be decreased to save RAM
 parser.add_argument('--ndf', dest='ndf', type=int, default=64, help='# of discri filters in first conv layer')
 parser.add_argument('--input_nc', dest='input_nc', type=int, default=1, help='# of input image channels')
 parser.add_argument('--output_nc', dest='output_nc', type=int, default=1, help='# of output image channels')
