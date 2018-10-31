239ccb8f69cd34ed2701b363ccb76b5d1bcd32f8
David Wajngot <davwa@cmiv.local>
2018-10-31 16:14:35 +0100

    testing and training with resized images and downsampling

--- CycleGAN/model.py
+++ CycleGAN/model.py
@@ -8,6 +8,7 @@ from collections import namedtuple
 from module import *
 from utils import *
 from shutil import copyfile
+from skimage.transform import resize
 
 class cyclegan(object):
     def __init__(self, sess, args):
@@ -149,12 +150,6 @@ class cyclegan(object):
                 batch_files = list(zip(dataA[idx * self.batch_size:(idx + 1) * self.batch_size],
                                        dataB[idx * self.batch_size:(idx + 1) * self.batch_size]))
 
-                '''# added check for thumbs.db in A and B
-                batch_images = []
-                for batch_file in batch_files:
-                     original_A, AB_image = load_train_data(batch_file, args.load_size, args.fine_size, args.input_nc, args.output_nc)
-                     batch_images.append(AB_image)'''
-
                 batch_images = [load_train_data(batch_file, args.load_size, args.fine_size, args.input_nc, args.output_nc) \
                 for batch_file in batch_files]
                 batch_images = np.array(batch_images).astype(np.float32)
@@ -167,10 +162,11 @@ class cyclegan(object):
                 #########################
                 # Save images for cnr and snr calculations in matlab
                 #if epoch % 4 == 0:
+                print ("idx:", idx)
                 '''print("saving batch", idx)
                 path = "../MATLAB/to_matlab/"
                 for i in range(len(batch_files)):
-                    print(i, batch_files[i][0])
+                    #print(i, batch_files[i][0])
                     file_name = batch_files[i][0].rsplit("\\", 1)
                     file_name = file_name[1]
                     #print("fn:", file_name)
@@ -179,8 +175,13 @@ class cyclegan(object):
                     #print("original_path", original_path)
                     #print("fake_path:", fake_path)
                     copyfile(batch_files[i][0], original_path)
-                    #print("fake_B[i]:",fake_B[i].shape)
-                    resh = np.reshape(fake_B[i], (256, 256))
+                    #print("fake_B[i]:",fake_B[i].shape)'
+                    #remove color channel info to make image saveable
+                    resh = np.reshape(fake_B[i], (args.fine_size, args.fine_size))
+                    # RESIZE TO 256x256 ?
+                    # I think so if you want to perform calcs on it
+                    # To get SNR etc. 
+                    resh = resize(resh, (256,256), anti_aliasing=True)
                     scipy.misc.imsave(fake_path, resh)
                     batch_counter += 1'''
 
@@ -199,15 +200,15 @@ class cyclegan(object):
                 self.writer.add_summary(summary_str, counter)
                 
                 counter += 1        
-                print ("counter:", counter)
-                #Saves a sample image with print_freq
+                #Saves a sample image with print_freq (and prints info)
                 if np.mod(counter, args.print_freq) == 1:
-                    #self.sample_model(args.sample_dir, epoch, idx)
+                    #self.sample_model(args.sample_dir, epoch, idx, args.load_size, args.fine_size)
                     print(("Epoch: [%2d] [%4d/%4d] time: %4.4f" % (
                         epoch, idx, batch_idxs, time.time() - start_time)))   
                     print("SNR:",snr)
                     print("G_LOSS:", g_loss, "D_LOSS:", d_loss)
 
+                #Create a model checkpoint at save_freq
                 if np.mod(counter, args.save_freq) == 2:
                     self.save(args.checkpoint_dir, counter)
 
@@ -215,6 +216,7 @@ class cyclegan(object):
 
     def save(self, checkpoint_dir, step):
         model_name = "cyclegan.model"
+        #remove image size from directory name?
         model_dir = "%s_%s" % (self.dataset_dir, self.image_size)
         checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
 
@@ -228,24 +230,30 @@ class cyclegan(object):
     def load(self, checkpoint_dir):
         print(" [*] Reading checkpoint...")
 
+        #remove image size from directory name?
         model_dir = "%s_%s" % (self.dataset_dir, self.image_size)
         checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
 
-        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+        print("MODEL_DIR:", model_dir)
+        print("CHECKPOINT_DIR:", checkpoint_dir)
+
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir) #Analyze get_checkpoint_state!!
         if ckpt and ckpt.model_checkpoint_path:
             ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+            print("CHECKPOINT NAME:", ckpt_name)
             self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
             return True
         else:
             return False
 
-    def sample_model(self, sample_dir, epoch, idx):
+    def sample_model(self, sample_dir, epoch, idx, load_size, fine_size):
         dataA = glob('./datasets/{}/*.png*'.format(self.dataset_dir + '/testA'))
         dataB = glob('./datasets/{}/*.png*'.format(self.dataset_dir + '/testB'))
         np.random.shuffle(dataA)
         np.random.shuffle(dataB)
         batch_files = list(zip(dataA[:self.batch_size], dataB[:self.batch_size]))
-        sample_images = [load_train_data(batch_file, is_testing=True) for batch_file in batch_files]
+        sample_images = [load_train_data(batch_file, load_size=load_size, \
+            fine_size=fine_size, is_testing=True) for batch_file in batch_files]
         sample_images = np.array(sample_images).astype(np.float32)
 
         fake_A, fake_B = self.sess.run(
@@ -257,7 +265,7 @@ class cyclegan(object):
         save_images(fake_B, [self.batch_size, 1],
                     './{}/B_{:02d}_{:04d}.png'.format(sample_dir, epoch, idx))
 
-    def test(self, args):
+    def test(self, args, output_size=256):
         """Test cyclegan"""
         init_op = tf.global_variables_initializer()
         self.sess.run(init_op)
@@ -274,6 +282,7 @@ class cyclegan(object):
             print(" [*] Load SUCCESS")
         else:
             print(" [!] Load failed...")
+            return
 
         # write html for visual comparison
         index_path = os.path.join(args.test_dir, '{0}_index.html'.format(args.which_direction))
@@ -295,9 +304,16 @@ class cyclegan(object):
             image_path = os.path.join(args.test_dir,
                                       '{0}_{1}'.format(args.which_direction, os.path.basename(sample_file)))
             fake_img = self.sess.run(out_var, feed_dict={in_var: sample_image})
-            #Resize image
-            #But will probably only receive 256x256
-            save_images(fake_img, [1, 1], image_path)
+            
+            #Resize image to 256x256
+            n_of_images = fake_img.shape[0]
+            for i in range(n_of_images):
+                fake_resized = resize(fake_img[0], (output_size,output_size), anti_aliasing=True)
+                fake_resized = np.reshape(fake_resized, (output_size, output_size)) #get rid of color channel
+                #print("FAKE_REIZED2:", fake_resized.shape)
+                scipy.misc.imsave(image_path, fake_resized)
+
+            #save_images(fake_img, [1, 1], image_path)
             index.write("<td>%s</td>" % os.path.basename(image_path))
             index.write("<td><img src='%s'></td>" % (sample_file if os.path.isabs(sample_file) else (
                 '..' + os.path.sep + sample_file)))
