ac9ea0b5eb393a28253d4d03cf00f8a36447f13e
David Wajngot <davwa@cmiv.local>
2018-12-06 18:01:59 +0100

    changed results calculation for less outliers

--- MATLAB/matlab_scripts/do_plot.m
+++ MATLAB/matlab_scripts/do_plot.m
@@ -1,17 +1,67 @@
-function do_plot(vec1,nr,tit,ylab)
+function do_plot(super,SNR_vector, ratio_SNR_vector,CNR_vector,ratio_CNR_vector)
 %function do_plot(vec1,nr,tit,ylab,xlab)
 
-x = ones(length(vec1),1);
-for i = 1:length(x)
-    x(i) = i;
-end
+x = (1:length(SNR_vector))';
 
-trend = fit(x,vec1,'poly2');
+figure
+title('Average values')
 
-figure(nr)
-plot(vec1);
+subplot(2,2,1);
+trend = fit(x,SNR_vector,'poly2');
+plot(SNR_vector);
 hold on
-plot(trend, x, vec1);
-title(tit)
+plot(trend,x,SNR_vector)
+title('SNR Increase');
 xlabel('Epochs')
-ylabel(ylab)
+ylabel('Increase')
+legend('off')
+
+subplot(2,2,2);
+trend = fit(x,ratio_SNR_vector,'poly2');
+plot(ratio_SNR_vector)
+hold on
+plot(trend,x,ratio_SNR_vector)
+title('SNR Percentual Increase');
+xlabel('Epochs')
+ylabel('Percentual Increase')
+legend('off')
+
+subplot(2,2,3);
+trend = fit(x,CNR_vector,'poly2');
+plot(CNR_vector)
+hold on
+plot(trend,x,CNR_vector)
+title('CNR Increase');
+xlabel('Epochs')
+ylabel('Increase')
+legend('off')
+
+subplot(2,2,4);
+trend = fit(x,ratio_CNR_vector,'poly2');
+plot(ratio_CNR_vector)
+hold on
+plot(trend,x,ratio_CNR_vector)
+title('CNR Percentual Increase');
+xlabel('Epochs')
+ylabel('Percentual Increase')
+legend('off')
+suptitle(super)
+
+
+
+
+%%
+% x = ones(length(vec1),1);
+% for i = 1:length(x)
+%     x(i) = i;
+% end
+% 
+% trend = fit(x,vec1,'poly2');
+% 
+% figure(nr)
+% plot(vec1);
+% hold on
+% plot(trend, x, vec1);
+% title(tit)
+% xlabel('Epochs')
+% ylabel(ylab)
--- MATLAB/matlab_scripts/results.m
+++ MATLAB/matlab_scripts/results.m
@@ -6,20 +6,20 @@
 %Get images and sort after date modified
 % originals = dir('../to_matlab/origs_terrible/*.png');
 % fakes = dir('../to_matlab/fakes_terrible/*.png');
-originals = dir('C:\Users\davwa\Desktop\CT-image-enhancement\CycleGAN\datasets\Sample_Quality\testA/*.png');
-fakes = dir('C:\Users\davwa\Desktop\CT-image-enhancement\CycleGAN\test_Sample_Quality/*.png');
+originals = dir('C:\Users\davwa\Desktop\CT-image-enhancement\CycleGAN\datasets\Full_Quality\testA/*.png');
+fakes = dir('C:\Users\davwa\Desktop\CT-image-enhancement\CycleGAN\test_Full_Quality/*.png');
 test = true; 
 [originals, fakes, L] = get_data(originals, fakes, test);
 
 %%%%% Testing
 %% 
 n = 1812;
-figure(80)
+figure
 orig = originals(n).name
 orig_path = strcat('C:\Users\davwa\Desktop\CT-image-enhancement\CycleGAN\datasets\Sample_Quality\testA/', orig);
 imshow(orig_path)
 
-figure(81)
+figure
 fake = fakes(n).name
 fakepath = strcat('C:\Users\davwa\Desktop\CT-image-enhancement\CycleGAN\test_Sample_Quality/', fake);
 imshow(fakepath)
@@ -40,29 +40,30 @@ images_per_epoch = 1817;
 n_of_epochs = floor(L/images_per_epoch); %data sampled from X epochs 
 
 %For BA, takes all values from one (last) epoch
-orig_SNR_vector = zeros(images_per_epoch,1);
-fake_SNR_vector = zeros(images_per_epoch,1);
-orig_CNR_vector = zeros(images_per_epoch,1);
-fake_CNR_vector = zeros(images_per_epoch,1);
+% orig_SNR_vector = zeros(images_per_epoch,1);
+% fake_SNR_vector = zeros(images_per_epoch,1);
+% orig_CNR_vector = zeros(images_per_epoch,1);
+% fake_CNR_vector = zeros(images_per_epoch,1);
 
-SNR_vector = zeros(n_of_epochs,1);
-ratio_SNR_vector = zeros(n_of_epochs,1);
-CNR_vector = zeros(n_of_epochs,1);
-ratio_CNR_vector = zeros(n_of_epochs,1);
-UIQI_vector = zeros(n_of_epochs,1);
+%all right now just only gather all the data. no calculations
+SNR_diff_vector = zeros(n_of_epochs*images_per_epoch,1);
+SNR_ratio_vector = zeros(n_of_epochs*images_per_epoch,1);
+CNR_diff_vector = zeros(n_of_epochs*images_per_epoch,1);
+CNR_ratio_vector = zeros(n_of_epochs*images_per_epoch,1);
+UIQI_vector = zeros(n_of_epochs*images_per_epoch,1);
 
-SNR_epoch = 0;
-ratio_SNR_epoch = 0;
-CNR_epoch = 0;
-ratio_CNR_epoch = 0;
-UIQI_epoch = 0;
+% SNR_epoch = 0;
+% ratio_SNR_epoch = 0;
+% CNR_epoch = 0;
+% ratio_CNR_epoch = 0;
+% UIQI_epoch = 0;
 
 % nan1index_vector = [];
 % nan1ame_vector = [];
 % nan2index_vector = [];
 % nan2ame_vector = [];
 
-%%%%%%%%%%%%%% GIANT FOR LOOP, FILL VECTORS %%%%%%%%%%%%
+%%%%%%%%%%%%%% FOR LOOP FOR GETTING THE VECTOR VALUES %%%%%%%%%%%%
 %%
 size = 256;
 epoch = 1;
@@ -77,14 +78,14 @@ for i = 1:L
     else
         orig_name = originals(i).name;
     end
-    orig_path = strcat('C:\Users\davwa\Desktop\CT-image-enhancement\CycleGAN\datasets\Sample_Quality\testA/', orig_name); 
+    orig_path = strcat('C:\Users\davwa\Desktop\CT-image-enhancement\CycleGAN\datasets\Full_Quality\testA/', orig_name); 
     orig = get_image(orig_path);
     orig_outside = get_outside(orig, size, size);
     [orig_SNR,orig_CNR] = get_SNR_CNR(orig,orig_outside,size,size);
     %orig_SNR
     % Get fake
     fake_name = fakes(i).name;
-    fake_path = strcat('C:\Users\davwa\Desktop\CT-image-enhancement\CycleGAN\test_Sample_Quality/', fake_name);
+    fake_path = strcat('C:\Users\davwa\Desktop\CT-image-enhancement\CycleGAN\test_Full_Quality/', fake_name);
     fake = get_image(fake_path);
     [fake_SNR,fake_CNR] = get_SNR_CNR(fake,orig_outside,size,size);
 %     fake_SNR
@@ -96,8 +97,10 @@ for i = 1:L
     if sign(SNR_diff) ~= sign(SNR_ratio)
         SNR_ratio = SNR_ratio * -1;
     end
-    SNR_epoch = SNR_epoch + SNR_diff;
-    ratio_SNR_epoch = ratio_SNR_epoch + SNR_ratio;
+%     SNR_epoch = SNR_epoch + SNR_diff;
+%     ratio_SNR_epoch = ratio_SNR_epoch + SNR_ratio;
+    SNR_diff_vector(i) = SNR_diff;
+    SNR_ratio_vector(i) = SNR_ratio;
 %     else
 %         nan1index_vector = [nan1index_vector, i];
 %         nan1ame_vector= [nan1ame_vector, orig_name];
@@ -110,8 +113,10 @@ for i = 1:L
     if sign(CNR_diff) ~= sign(CNR_ratio)
         CNR_ratio = CNR_ratio * -1;
     end
-    CNR_epoch = CNR_epoch + CNR_diff;
-    ratio_CNR_epoch = ratio_CNR_epoch + CNR_ratio;
+%     CNR_epoch = CNR_epoch + CNR_diff;
+%     ratio_CNR_epoch = ratio_CNR_epoch + CNR_ratio;
+    CNR_diff_vector(i) = CNR_diff;
+    CNR_ratio_vector(i) = CNR_ratio;
 %     else
 %         nan2index_vector = [nan2index_vector, i];
 %         nan2ame_vector= [nan2ame_vector, orig_name];
@@ -119,50 +124,106 @@ for i = 1:L
 %     end
     % UIQI
     [UIQI, ~] = get_uiqi(orig, fake);
-    UIQI_epoch = UIQI_epoch + UIQI;
+%     UIQI_epoch = UIQI_epoch + UIQI;
+    UIQI_vector(i) = UIQI;
 
-    if mod(i,images_per_epoch) == 0 % End of epoch?
-        %CALCULATE MEAN
-        mean_SNR = SNR_epoch / images_per_epoch;
-        mean_SNR_ratio = ratio_SNR_epoch / images_per_epoch;
-        mean_CNR = CNR_epoch / images_per_epoch;
-        mean_CNR_ratio = ratio_CNR_epoch / images_per_epoch;
-        mean_UIQI = UIQI_epoch / images_per_epoch;
-        %ADD TO VECTOR
-        SNR_vector(epoch) = mean_SNR;
-        ratio_SNR_vector(epoch) = mean_SNR_ratio;
-        CNR_vector(epoch) = mean_CNR;
-        ratio_CNR_vector(epoch) = mean_CNR_ratio;
-        UIQI_vector(epoch) = mean_UIQI;
-        %RESET EPOCH VALUE
-        SNR_epoch = 0;
-        ratio_SNR_epoch = 0;
-        CNR_epoch = 0;
-        ratio_CNR_epoch = 0;
-        UIQI_epoch = 0;
-        %STEP EPOCH
-        epoch = epoch + 1;
-    end
+end
     
-    %If it is the last epoch, start saving for BA
-    if i > (L-images_per_epoch+1)
-        orig_SNR_vector(j) = orig_SNR;
-        fake_SNR_vector(j) = fake_SNR;
-        orig_CNR_vector(j) = orig_CNR;
-        fake_CNR_vector(j) = fake_CNR;
-        j = j + 1;
-    end
+%%%%% FOR LOOP FOR CALCULATING MEANS/MEDIANS FOR EACH EPOCH %%%%%
+%%
+SNR_diff_avg_vector = zeros(n_of_epochs,1);
+SNR_ratio_avg_vector = zeros(n_of_epochs,1);
+CNR_diff_avg_vector = zeros(n_of_epochs,1);
+CNR_ratio_avg_vector = zeros(n_of_epochs,1);
+UIQI_avg_vector = zeros(n_of_epochs,1);
+
+SNR_diff_med_vector=zeros(images_per_epoch,1);
+SNR_ratio_med_vector=zeros(images_per_epoch,1);
+CNR_diff_med_vector=zeros(images_per_epoch,1);
+CNR_ratio_med_vector=zeros(images_per_epoch,1);
+UIQI_med_vector = zeros(n_of_epochs,1);
+
+for i=1:n_of_epochs
+    i
+    SNR_diff_epoch = SNR_diff_vector(i:i+images_per_epoch);
+    SNR_diff_avg_vector(i) = mean(SNR_diff_epoch);
+    SNR_diff_med_vector(i) = median(SNR_diff_epoch);
+        
+    SNR_ratio_epoch = SNR_ratio_vector(i:i+images_per_epoch);
+    SNR_ratio_avg_vector(i) = mean(SNR_ratio_epoch);
+    SNR_ratio_med_vector(i) = median(SNR_ratio_epoch);
+    
+    CNR_diff_epoch = CNR_diff_vector(i:i+images_per_epoch);
+    CNR_diff_avg_vector(i) = mean(CNR_diff_epoch);
+    CNR_diff_med_vector(i) = median(CNR_diff_epoch);
+    
+    CNR_ratio_epoch = CNR_ratio_vector(i:i+images_per_epoch);
+    CNR_ratio_avg_vector(i) = mean(CNR_ratio_epoch);
+    CNR_ratio_med_vector(i) = median(CNR_ratio_epoch);
+        
+    UIQI_epoch = UIQI_vector(i:i+images_per_epoch);
+    UIQI_avg_vector(i) = mean(UIQI_epoch);
+    UIQI_med_vector(i) = median(UIQI_epoch);
+    
+%     if mod(i,images_per_epoch) == 0 % End of epoch?
+%         %CALCULATE MEAN
+% %         mean_SNR = SNR_epoch / images_per_epoch;
+% %         mean_SNR_ratio = ratio_SNR_epoch / images_per_epoch;
+% %         mean_CNR = CNR_epoch / images_per_epoch;
+% %         mean_CNR_ratio = ratio_CNR_epoch / images_per_epoch;
+%         median_SNR = median(SNR_diff_med_vector);
+%         median_SNR_ratio = median(SNR_ratio_med_vector);
+%         median_CNR = median(CNR_diff_med_vector);
+%         median_CNR_ratio = median(CNR_ratio_med_vector);
+%         
+%         mean_UIQI = UIQI_epoch / images_per_epoch;
+%         %ADD TO VECTOR
+% %         SNR_vector(epoch) = mean_SNR;
+% %         ratio_SNR_vector(epoch) = mean_SNR_ratio;
+% %         CNR_vector(epoch) = mean_CNR;
+% %         ratio_CNR_vector(epoch) = mean_CNR_ratio;
+%         SNR_diff_vector(epoch) = median_SNR;
+%         SNR_ratio_vector(epoch) = median_SNR_ratio;
+%         CNR_diff_vector(epoch) = median_CNR;
+%         CNR_ratio_vector(epoch) = median_CNR_ratio;
+%         UIQI_vector(epoch) = mean_UIQI;
+%         %RESET EPOCH VALUE
+% %         SNR_epoch = 0;
+% %         ratio_SNR_epoch = 0;
+% %         CNR_epoch = 0;
+% %         ratio_CNR_epoch = 0;
+%         SNR_diff_med_vector=zeros(images_per_epoch,1);
+%         SNR_ratio_med_vector=zeros(images_per_epoch,1);
+%         CNR_diff_med_vector=zeros(images_per_epoch,1);
+%         CNR_ratio_med_vector=zeros(images_per_epoch,1);
+%         UIQI_epoch = 0;
+%         %STEP EPOCH
+%         epoch = epoch + 1;
+%     end
+%     
+%     %If it is the last epoch, start saving for BA
+%     if i > (L-images_per_epoch+1)
+%         orig_SNR_vector(j) = orig_SNR;
+%         fake_SNR_vector(j) = fake_SNR;
+%         orig_CNR_vector(j) = orig_CNR;
+%         fake_CNR_vector(j) = fake_CNR;
+%         j = j + 1;
+%     end
 end
 
 %%%%%%%%%%%%%% PLOT RESULTS WITH TRENDS%%%%%%%%%%%%
 %%
-%maybe redo this to one single function call
 % close all;
+%mean
+do_plot('Average values',SNR_diff_avg_vector,SNR_ratio_avg_vector,CNR_diff_avg_vector,CNR_ratio_avg_vector);
+%median
+do_plot('Median values', SNR_diff_med_vector,SNR_ratio_med_vector,CNR_diff_med_vector,CNR_ratio_med_vector);
 
-do_plot(SNR_vector , 11, 'SNR', 'SNR difference');
-do_plot(ratio_SNR_vector, 22, 'SNR ratio', 'Percentage development');
-do_plot(CNR_vector, 33, 'CNR', 'CNR difference');
-do_plot(ratio_CNR_vector, 44, 'CNR ratio', 'Percentage development');
+
+% do_plot(SNR_vector , 11, 'SNR', 'SNR difference');
+% do_plot(ratio_SNR_vector, 22, 'SNR ratio', 'Percentage development');
+% do_plot(CNR_vector, 33, 'CNR', 'CNR difference');
+% do_plot(ratio_CNR_vector, 44, 'CNR ratio', 'Percentage development');
 % do_plot(UIQI_vector, 77, 'UIQI', 'UIQI');
 
 %%%%%%%%%%%%%% BLAND ALTMAN AND CORRELATION %%%%%%%%%%%%
@@ -205,6 +266,6 @@ ylabel('CNR ratio')
 %Save workspace
 total_epochs = 80;
 saved_every = 1;
-save('sample_R_test', 'total_epochs', 'saved_every', 'images_per_epoch', ...
+save('full_R_test_median', 'total_epochs', 'saved_every', 'images_per_epoch', ...
     'SNR_vector', 'CNR_vector', 'ratio_SNR_vector', 'ratio_CNR_vector',... 
     'UIQI_vector', 'orig_SNR_vector', 'fake_SNR_vector', 'orig_CNR_vector', 'fake_CNR_vector')
