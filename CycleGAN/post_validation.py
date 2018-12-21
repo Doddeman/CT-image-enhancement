851f0d312fa9fb85f72fad4b5df723b84bd7cb15
David Wajngot <davwa@cmiv.local>
2018-12-21 14:57:56 +0100

    removed comment

--- CycleGAN/post_validation.py
+++ CycleGAN/post_validation.py
@@ -1,10 +1,5 @@
 import os
 
-#make this into cross validation?
-#In that case, split the validation set into e.g. 5
-#equally large parts. Use one of the parts randomly
-#for each epoch
-
 for i in range(0,80,5): #How many epochs/checkpoints?
 	print("\n STARTING NEW TEST")
 	print("python main.py --phase=test --checkpoint=%d" % i)
